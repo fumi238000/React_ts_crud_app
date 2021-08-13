e6f42e1bcb3384d2154245d6b6dadf34e84e7e61
fumi238000 <s21231128@gmail.com>
2021-08-13 18:33:37 +0900

    [add]create_post_table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,27 @@ user_params = [
 User.delete_all
 User.create!(user_params)
 puts "ユーザーの初期データの投入に成功しました!"
+
+post_params = [
+  {
+    user_id: 1,
+    title: "React",
+    details: "詳細はこちらになります"
+  },
+  {
+    user_id: 1,
+    title: "Vue.js",
+    details: "詳細はこちらになります"
+  },
+  {
+    user_id: 1,
+    title: "Angular",
+    details: "詳細はこちらになります"
+  }
+]
+
+Post.delete_all
+Post.create!(post_params)
+puts "Postの初期データの投入に成功しました!"
+
+puts "すべての初期データ投入に成功しました!"
