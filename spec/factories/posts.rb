25f0714aefac0ca4350127d04959108194f0a265
fumi238000 <s21231128@gmail.com>
2021-08-17 18:59:35 +0900

    [add]post#index_test

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :post do
-    user { nil }
-    title { "MyString" }
-    details { "MyText" }
+    association :user, factory: :user
+    title { Faker::Lorem.characters(number: 8) }
+    details { Faker::Lorem.characters(number: 30) }
+
+    trait :invalid do
+      title { nil }
+      details { nil }
+    end
   end
 end
--- spec/requests/v1/posts_spec.rb
+++ spec/requests/v1/posts_spec.rb
@@ -1,7 +1,34 @@
 require "rails_helper"
 
 RSpec.describe "V1::Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:current_user) { create(:user) }
+  let(:headers) { current_user.create_new_auth_token }
+  let(:user) { create(:user) }
+
+  describe "GET #index" do
+    subject { get(v1_posts_path, headers: headers) }
+
+    context "トークン認証情報がない場合" do
+      subject { get(v1_posts_path) }
+      let!(:post) { create(:post, user_id: current_user.id) }
+      xit "エラーする" do
+        subject
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    before { create_list(:post, 3, user_id: current_user.id) }
+    context "ユーザーの投稿が存在するとき" do
+      it "投稿一覧を取得できること" do
+        subject
+        json = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(json.size).to eq 3
+        expect(json[0].keys).to eq %w[id title details]
+        expect(json[0]["id"]).to eq Post.first.id
+        expect(json[0]["title"]).to eq Post.first.title
+        expect(json[0]["details"]).to eq Post.first.details
+      end
+    end
   end
 end
