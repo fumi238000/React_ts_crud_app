abf9a6905df15429c219dd3a551fbcc4c6e11a38
fumi238000 <s21231128@gmail.com>
2021-08-23 08:12:41 +0900

    change style for button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router/Router';
 import './App.css';
@@ -7,7 +9,7 @@ import { PostProvider } from './providers/PostProvider';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <PostProvider>
       <BrowserRouter>
         <h1>TODOアプリ</h1>
