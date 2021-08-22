08e781da70eec085dfe0b5c3a8008aa4012ff52e
fumi238000 <s21231128@gmail.com>
2021-08-23 07:59:34 +0900

    install chakra UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router/Router';
 import './App.css';
@@ -6,12 +7,14 @@ import { PostProvider } from './providers/PostProvider';
 
 function App() {
   return (
+    <ChakraProvider>
     <PostProvider>
       <BrowserRouter>
         <h1>TODOアプリ</h1>
         <Router/>
       </BrowserRouter>
     </PostProvider>
+    </ChakraProvider>
   );
 }
 
