304679aa39799d6d3e452434e200e6cd3285aca1
fumi238000 <s21231128@gmail.com>
2021-08-31 08:29:21 +0900

    fix Page404 of layout

--- frontend/src/components/pages/Page404.tsx
+++ frontend/src/components/pages/Page404.tsx
@@ -1,7 +1,19 @@
+import { Flex, Heading } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Page404: VFC = memo(() => {
-  return <p>404ページです</p>
-});
+  return (
+    <>
+    <Flex align="center" justify="center" height="60vh">
+      <Heading as="h1"
+               size="2xl"
+               textAlign="center"
+               py={4}
+      >
+        PAGE 404
+      </Heading>
+      </Flex>
+    </>
+)});
   
 
