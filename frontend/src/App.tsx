15fff28532d627b6d97b7df7fa31314d1d863f84
fumi238000 <s21231128@gmail.com>
2021-09-01 09:22:18 +0900

    fix layouts_padding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,22 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import theme from './theme/theme';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router/Router';
 import './App.css';
+import { HeaderLayout } from './components/templates/HeaderLayout';
 
 function App() {
-
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <Router/>
+        <HeaderLayout>
+          <Box m={16}>
+            <Router />
+          </Box>
+        </HeaderLayout>
       </BrowserRouter>
     </ChakraProvider>
   );
 }
-
 export default App;
