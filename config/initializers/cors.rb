5f524816c61c561f40071e03d53958cc069f4513
fumi238000 <s21231128@gmail.com>
2021-09-25 18:51:49 +0900

    ready for first push to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "https://react-todo-2021-9-2.herokuapp.com/api"
+    origins "http://localhost:3001", "https://react-todo-2021-9-2.herokuapp.com/v1"
 
     resource "*",
              headers: :any,
